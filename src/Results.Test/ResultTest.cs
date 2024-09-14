ffcbdfaecb715e5c3da620de0455ef445b2c8ab7
ap-dev-at-home <AndreeP@gmx.de>
2024-09-14 17:44:52 +0200

    Add UnitTests

--- src/Results.Test/ResultTest.cs
+++ src/Results.Test/ResultTest.cs
@@ -90,6 +90,233 @@ public class ResultTests
     }
 
     [TestMethod]
+    public void ResultTryExecutesAction()
+    {
+        var assertTry = new AssertFlagPassthrough();
+        var assertCatch = new AssertFlagPassthrough();
+
+        var result = Result.Try(() =>
+        {
+            assertTry.Assert(() => true);
+        }, (Exception ex) =>
+        {
+            assertCatch.Assert(() => true);
+        });
+
+        Assert.AreEqual(true, result.Success);
+        assertTry.Assert(true);
+        assertCatch.Assert(false);
+    }
+
+    [TestMethod]
+    public void ResultTryActionExecutesCatch()
+    {
+        var assertTry = new AssertFlagPassthrough();
+        var assertCatch = new AssertFlagPassthrough();
+
+        var result = Result.Try(() =>
+        {
+            assertTry.Assert(() => true);
+            throw new Exception("Exception was thrown");
+        }, (Exception ex) =>
+        {
+            assertCatch.Assert(() => true);
+            Assert.AreEqual("Exception was thrown", ex.Message);
+        });
+
+        Assert.AreEqual(false, result.Success);
+        Assert.AreEqual(1, result.Errors.Count);
+        Assert.IsInstanceOfType<ExceptionError>(result.Errors[0]);
+        Assert.AreEqual("Exception was thrown", ((ExceptionError)result.Errors[0]).Exception.Message);
+        assertTry.Assert(true);
+        assertCatch.Assert(true);
+    }
+
+    [TestMethod]
+    public void ResultTryT1ExecutesAction()
+    {
+        var assertTry = new AssertFlagPassthrough();
+        var assertCatch = new AssertFlagPassthrough();
+
+        var result = Result.Try((i) =>
+        {
+            assertTry.Assert(() => i == 1);
+        }, 1, (Exception ex) =>
+        {
+            assertCatch.Assert(() => true);
+        });
+
+        Assert.AreEqual(true, result.Success);
+        assertTry.Assert(true);
+        assertCatch.Assert(false);
+    }
+
+    [TestMethod]
+    public void ResultTryActionT1ExecutesCatch()
+    {
+        var assertTry = new AssertFlagPassthrough();
+        var assertCatch = new AssertFlagPassthrough();
+
+        var result = Result.Try((i) =>
+        {
+            assertTry.Assert(() => i == 1);
+            throw new Exception("Exception was thrown");
+        }, 1, (Exception ex) =>
+        {
+            assertCatch.Assert(() => true);
+            Assert.AreEqual("Exception was thrown", ex.Message);
+        });
+
+        Assert.AreEqual(false, result.Success);
+        Assert.AreEqual(1, result.Errors.Count);
+        Assert.IsInstanceOfType<ExceptionError>(result.Errors[0]);
+        Assert.AreEqual("Exception was thrown", ((ExceptionError)result.Errors[0]).Exception.Message);
+        assertTry.Assert(true);
+        assertCatch.Assert(true);
+    }
+
+    [TestMethod]
+    public void ResultTryT2ExecutesAction()
+    {
+        var assertTry = new AssertFlagPassthrough();
+        var assertCatch = new AssertFlagPassthrough();
+
+        var result = Result.Try((i, j) =>
+        {
+            assertTry.Assert(() => i == 1);
+            assertTry.Assert(() => j == 2);
+        }, 1, 2, (Exception ex) =>
+        {
+            assertCatch.Assert(() => true);
+        });
+
+        Assert.AreEqual(true, result.Success);
+        assertTry.Assert(true);
+        assertCatch.Assert(false);
+    }
+
+    [TestMethod]
+    public void ResultTryActionT2ExecutesCatch()
+    {
+        var assertTry = new AssertFlagPassthrough();
+        var assertCatch = new AssertFlagPassthrough();
+
+        var result = Result.Try((i, j) =>
+        {
+            assertTry.Assert(() => i == 1);
+            assertTry.Assert(() => j == 2);
+            throw new Exception("Exception was thrown");
+        }, 1, 2, (Exception ex) =>
+        {
+            assertCatch.Assert(() => true);
+            Assert.AreEqual("Exception was thrown", ex.Message);
+        });
+
+        Assert.AreEqual(false, result.Success);
+        Assert.AreEqual(1, result.Errors.Count);
+        Assert.IsInstanceOfType<ExceptionError>(result.Errors[0]);
+        Assert.AreEqual("Exception was thrown", ((ExceptionError)result.Errors[0]).Exception.Message);
+        assertTry.Assert(true);
+        assertCatch.Assert(true);
+    }
+
+    [TestMethod]
+    public void ResultTryT3ExecutesAction()
+    {
+        var assertTry = new AssertFlagPassthrough();
+        var assertCatch = new AssertFlagPassthrough();
+
+        var result = Result.Try((i, j, k) =>
+        {
+            assertTry.Assert(() => i == 1);
+            assertTry.Assert(() => j == 2);
+            assertTry.Assert(() => k == 3);
+        }, 1, 2, 3, (Exception ex) =>
+        {
+            assertCatch.Assert(() => true);
+        });
+
+        Assert.AreEqual(true, result.Success);
+        assertTry.Assert(true);
+        assertCatch.Assert(false);
+    }
+
+    [TestMethod]
+    public void ResultTryActionT3ExecutesCatch()
+    {
+        var assertTry = new AssertFlagPassthrough();
+        var assertCatch = new AssertFlagPassthrough();
+
+        var result = Result.Try((i, j, k) =>
+        {
+            assertTry.Assert(() => i == 1);
+            assertTry.Assert(() => j == 2);
+            assertTry.Assert(() => k == 3);
+            throw new Exception("Exception was thrown");
+        }, 1, 2, 3, (Exception ex) =>
+        {
+            assertCatch.Assert(() => true);
+            Assert.AreEqual("Exception was thrown", ex.Message);
+        });
+
+        Assert.AreEqual(false, result.Success);
+        Assert.AreEqual(1, result.Errors.Count);
+        Assert.IsInstanceOfType<ExceptionError>(result.Errors[0]);
+        Assert.AreEqual("Exception was thrown", ((ExceptionError)result.Errors[0]).Exception.Message);
+        assertTry.Assert(true);
+        assertCatch.Assert(true);
+    }
+
+    [TestMethod]
+    public void ResultTryT4ExecutesAction()
+    {
+        var assertTry = new AssertFlagPassthrough();
+        var assertCatch = new AssertFlagPassthrough();
+
+        var result = Result.Try((i, j, k, l) =>
+        {
+            assertTry.Assert(() => i == 1);
+            assertTry.Assert(() => j == 2);
+            assertTry.Assert(() => k == 3);
+            assertTry.Assert(() => l == 4);
+        }, 1, 2, 3, 4, (Exception ex) =>
+        {
+            assertCatch.Assert(() => true);
+        });
+
+        Assert.AreEqual(true, result.Success);
+        assertTry.Assert(true);
+        assertCatch.Assert(false);
+    }
+
+    [TestMethod]
+    public void ResultTryActionT4ExecutesCatch()
+    {
+        var assertTry = new AssertFlagPassthrough();
+        var assertCatch = new AssertFlagPassthrough();
+
+        var result = Result.Try((i, j, k, l) =>
+        {
+            assertTry.Assert(() => i == 1);
+            assertTry.Assert(() => j == 2);
+            assertTry.Assert(() => k == 3);
+            assertTry.Assert(() => l == 4);
+            throw new Exception("Exception was thrown");
+        }, 1, 2, 3, 4, (Exception ex) =>
+        {
+            assertCatch.Assert(() => true);
+            Assert.AreEqual("Exception was thrown", ex.Message);
+        });
+
+        Assert.AreEqual(false, result.Success);
+        Assert.AreEqual(1, result.Errors.Count);
+        Assert.IsInstanceOfType<ExceptionError>(result.Errors[0]);
+        Assert.AreEqual("Exception was thrown", ((ExceptionError)result.Errors[0]).Exception.Message);
+        assertTry.Assert(true);
+        assertCatch.Assert(true);
+    }
+
+    [TestMethod]
     public void ResultTryExecutesFunc()
     {
         var assertTry = new AssertFlagPassthrough();
