6d74a69fdf243f5a5c289a4347f031f74ef65839
ap-dev-at-home <AndreeP@gmx.de>
2024-09-26 22:50:47 +0200

    Add async equivalents for Do methods

--- src/Results/Result.cs
+++ src/Results/Result.cs
@@ -80,6 +80,56 @@ public class Result
     }
 
     /// <summary>
+    /// Executes an asynchronous function without parameters.
+    /// </summary>
+    /// <param name="func">The asynchronous function to execute.</param>
+    /// <returns>A task representing the result returned from func.</returns>
+    public static async Task<Result<TResult>> DoAsync<TResult>(Func<Task<Result<TResult>>> func)
+    {
+        return await func();
+    }
+
+    /// <summary>
+    /// Executes an asynchronous function with 1 parameter.
+    /// </summary>
+    /// <param name="func">The asynchronous function to execute.</param>
+    /// <returns>A task representing the result returned from func.</returns>
+    public static async Task<Result<TResult>> DoAsync<T1, TResult>(Func<T1, Task<Result<TResult>>> func, T1 arg1)
+    {
+        return await func(arg1);
+    }
+
+    /// <summary>
+    /// Executes an asynchronous function with 2 parameters.
+    /// </summary>
+    /// <param name="func">The asynchronous function to execute.</param>
+    /// <returns>A task representing the result returned from func.</returns>
+    public static async Task<Result<TResult>> DoAsync<T1, T2, TResult>(Func<T1, T2, Task<Result<TResult>>> func, T1 arg1, T2 arg2)
+    {
+        return await func(arg1, arg2);
+    }
+
+    /// <summary>
+    /// Executes an asynchronous function with 3 parameters.
+    /// </summary>
+    /// <param name="func">The asynchronous function to execute.</param>
+    /// <returns>A task representing the result returned from func.</returns>
+    public static async Task<Result<TResult>> DoAsync<T1, T2, T3, TResult>(Func<T1, T2, T3, Task<Result<TResult>>> func, T1 arg1, T2 arg2, T3 arg3)
+    {
+        return await func(arg1, arg2, arg3);
+    }
+
+    /// <summary>
+    /// Executes an asynchronous function with 4 parameters.
+    /// </summary>
+    /// <param name="func">The asynchronous function to execute.</param>
+    /// <returns>A task representing the result returned from func.</returns>
+    public static async Task<Result<TResult>> DoAsync<T1, T2, T3, T4, TResult>(Func<T1, T2, T3, T4, Task<Result<TResult>>> func, T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+    {
+        return await func(arg1, arg2, arg3, arg4);
+    }
+
+    /// <summary>
     /// Executes an action without parameter and catches any exceptions.
     /// </summary>
     /// <param name="action">The action to execute.</param>
