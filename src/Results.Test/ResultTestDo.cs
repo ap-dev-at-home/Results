768d53039531dcb9c836d6e16441aa69bde607be
ap-dev-at-home <AndreeP@gmx.de>
2024-09-29 01:32:41 +0200

    Add async Do, Split Unittests, Add Unittests

---
+++ src/Results.Test/ResultTestDo.cs
@@ -0,0 +1,184 @@
+using System.Collections.ObjectModel;
+
+namespace Results.Test;
+
+[TestClass]
+public class ResultTestsDo
+{
+    [TestMethod]
+    public void ResultDoExecutesFunc()
+    {
+        var assert = new AssertFlagPassthrough();
+
+        var result = Result.Do(() => 
+        { 
+            assert.Assert(() => true);
+            return Result.Ok(1);
+        });
+        
+        Assert.AreEqual(true, result.Success);
+        Assert.AreEqual(1, result.Value);
+        assert.Assert(true, 1);
+    }
+
+    [TestMethod]
+    public void ResultDoT1ExecutesFunc()
+    {
+        var assert = new AssertFlagPassthrough();
+
+        var result = Result.Do((i) =>
+        {
+            assert.Assert(() => i == 1);
+            return Result.Ok(i);
+        }, 1);
+
+        Assert.AreEqual(true, result.Success);
+        Assert.AreEqual(1, result.Value);
+        assert.Assert(true, 1);
+    }
+
+    [TestMethod]
+    public void ResultDoT2ExecutesFunc()
+    {
+        var assert = new AssertFlagPassthrough();
+
+        var result = Result.Do((i, j) =>
+        {
+            assert.Assert(() => i == 1);
+            assert.Assert(() => j == 2);
+            return Result.Ok(i + j);
+        }, 1, 2);
+
+        Assert.AreEqual(true, result.Success);
+        Assert.AreEqual(1 + 2, result.Value);
+        assert.Assert(true, 2);
+    }
+
+    [TestMethod]
+    public void ResultDoT3ExecutesFunc()
+    {
+        var assert = new AssertFlagPassthrough();
+
+        var result = Result.Do((i, j, k) =>
+        {
+            assert.Assert(() => i == 1);
+            assert.Assert(() => j == 2);
+            assert.Assert(() => k == 3);
+            return Result.Ok(i + j + k);
+        }, 1, 2, 3);
+
+        Assert.AreEqual(true, result.Success);
+        Assert.AreEqual(1 + 2 + 3, result.Value);
+        assert.Assert(true, 3);
+    }
+
+    [TestMethod]
+    public void ResultDoT4ExecutesFunc()
+    {
+        var assert = new AssertFlagPassthrough();
+
+        var result = Result.Do((i, j, k, l) =>
+        {
+            assert.Assert(() => i == 1);
+            assert.Assert(() => j == 2);
+            assert.Assert(() => k == 3);
+            assert.Assert(() => l == 4);
+            return Result.Ok(i + j + k + l);
+        }, 1, 2, 3, 4);
+
+        Assert.AreEqual(true, result.Success);
+        Assert.AreEqual(1 + 2 + 3 + 4, result.Value);
+        assert.Assert(true, 4);
+    }
+
+    [TestMethod]
+    public async Task ResultDoAsyncExecutesFunc()
+    {
+        var assert = new AssertFlagPassthrough();
+
+        var result = await Result.DoAsync(async () =>
+        {
+            await Task.CompletedTask;
+            assert.Assert(() => true);
+            return Result.Ok(1);
+        });
+
+        Assert.AreEqual(true, result.Success);
+        Assert.AreEqual(1, result.Value);
+        assert.Assert(true, 1);
+    }
+
+    [TestMethod]
+    public async Task ResultDoAsyncT1ExecutesFunc()
+    {
+        var assert = new AssertFlagPassthrough();
+
+        var result = await Result.DoAsync(async (i) =>
+        {
+            await Task.CompletedTask;
+            assert.Assert(() => i == 1);
+            return Result.Ok(i);
+        }, 1);
+
+        Assert.AreEqual(true, result.Success);
+        Assert.AreEqual(1, result.Value);
+        assert.Assert(true, 1);
+    }
+
+    [TestMethod]
+    public async Task ResultDoAsyncT2ExecutesFunc()
+    {
+        var assert = new AssertFlagPassthrough();
+
+        var result = await Result.DoAsync(async (i, j) =>
+        {
+            await Task.CompletedTask;
+            assert.Assert(() => i == 1);
+            assert.Assert(() => j == 2);
+            return Result.Ok(i + j);
+        }, 1, 2);
+
+        Assert.AreEqual(true, result.Success);
+        Assert.AreEqual(1 + 2, result.Value);
+        assert.Assert(true, 2);
+    }
+
+    [TestMethod]
+    public async Task ResultDoAsyncT3ExecutesFunc()
+    {
+        var assert = new AssertFlagPassthrough();
+
+        var result = await Result.DoAsync(async (i, j, k) =>
+        {
+            await Task.CompletedTask;
+            assert.Assert(() => i == 1);
+            assert.Assert(() => j == 2);
+            assert.Assert(() => k == 3);
+            return Result.Ok(i + j + k);
+        }, 1, 2, 3);
+
+        Assert.AreEqual(true, result.Success);
+        Assert.AreEqual(1 + 2 + 3, result.Value);
+        assert.Assert(true, 3);
+    }
+
+    [TestMethod]
+    public async Task ResultDoAsyncT4ExecutesFunc()
+    {
+        var assert = new AssertFlagPassthrough();
+
+        var result = await Result.DoAsync(async (i, j, k, l) =>
+        {
+            await Task.CompletedTask;
+            assert.Assert(() => i == 1);
+            assert.Assert(() => j == 2);
+            assert.Assert(() => k == 3);
+            assert.Assert(() => l == 4);
+            return Result.Ok(i + j + k + l);
+        }, 1, 2, 3, 4);
+
+        Assert.AreEqual(true, result.Success);
+        Assert.AreEqual(1 + 2 + 3 + 4, result.Value);
+        assert.Assert(true, 4);
+    }
+}
--- src/Results/Result.cs
+++ src/Results/Result.cs
@@ -229,7 +229,6 @@ public class Result
         }
     }
 
-
     /// <summary>
     /// Executes a function with no parameters and catches any exceptions.
     /// </summary>
@@ -317,6 +316,101 @@ public class Result
         try
         {
             return func(arg1, arg2, arg3, arg4);
+        }
+        catch (Exception ex)
+        {
+            @catch?.Invoke(ex);
+            return Result.Fail<TResult>(new ExceptionError(ex));
+        }
+    }
+
+    /// <summary>
+    /// Asynchronously executes a function with no parameters and catches any exceptions.
+    /// </summary>
+    /// <param name="func">The asynchronous function to execute.</param>
+    /// <param name="catch">Action to call on exception.</param>
+    /// <returns>Failed result if exception occurs, otherwise a successful result with value.</returns>
+    public static async Task<Result<TResult>> TryAsync<TResult>(Func<Task<Result<TResult>>> func, Action<Exception>? @catch = null)
+    {
+        try
+        {
+            return await func();
+        }
+        catch (Exception ex)
+        {
+            @catch?.Invoke(ex);
+            return Result.Fail<TResult>(new ExceptionError(ex));
+        }
+    }
+
+    /// <summary>
+    /// Asynchronously executes a function with 1 parameter and catches any exceptions.
+    /// </summary>
+    /// <param name="func">The asynchronous function to execute.</param>
+    /// <param name="catch">Action to call on exception.</param>
+    /// <returns>Failed result if exception occurs, otherwise a successful result with value.</returns>
+    public static async Task<Result<TResult>> TryAsync<T1, TResult>(Func<T1, Task<Result<TResult>>> func, T1 arg1, Action<Exception>? @catch = null)
+    {
+        try
+        {
+            return await func(arg1);
+        }
+        catch (Exception ex)
+        {
+            @catch?.Invoke(ex);
+            return Result.Fail<TResult>(new ExceptionError(ex));
+        }
+    }
+
+    /// <summary>
+    /// Asynchronously executes a function with 2 parameters and catches any exceptions.
+    /// </summary>
+    /// <param name="func">The asynchronous function to execute.</param>
+    /// <param name="catch">Action to call on exception.</param>
+    /// <returns>Failed result if exception occurs, otherwise a successful result with value.</returns>
+    public static async Task<Result<TResult>> TryAsync<T1, T2, TResult>(Func<T1, T2, Task<Result<TResult>>> func, T1 arg1, T2 arg2, Action<Exception>? @catch = null)
+    {
+        try
+        {
+            return await func(arg1, arg2);
+        }
+        catch (Exception ex)
+        {
+            @catch?.Invoke(ex);
+            return Result.Fail<TResult>(new ExceptionError(ex));
+        }
+    }
+
+    /// <summary>
+    /// Asynchronously executes a function with 3 parameters and catches any exceptions.
+    /// </summary>
+    /// <param name="func">The asynchronous function to execute.</param>
+    /// <param name="catch">Action to call on exception.</param>
+    /// <returns>Failed result if exception occurs, otherwise a successful result with value.</returns>
+    public static async Task<Result<TResult>> TryAsync<T1, T2, T3, TResult>(Func<T1, T2, T3, Task<Result<TResult>>> func, T1 arg1, T2 arg2, T3 arg3, Action<Exception>? @catch = null)
+    {
+        try
+        {
+            return await func(arg1, arg2, arg3);
+        }
+        catch (Exception ex)
+        {
+            @catch?.Invoke(ex);
+            return Result.Fail<TResult>(new ExceptionError(ex));
+        }
+    }
+
+    /// <summary>
+    /// Asynchronously executes a function with 4 parameters and catches any exceptions.
+    /// </summary>
+    /// <param name="func">The asynchronous function to execute.</param>
+    /// <param name="catch">Action to call on exception.</param>
+    /// <returns>Failed result if exception occurs, otherwise a successful result with value.</returns>
+    public static async Task<Result<TResult>> TryAsync<T1, T2, T3, T4, TResult>(Func<T1, T2, T3, T4, Task<Result<TResult>>> func, T1 arg1, T2 arg2, T3 arg3, T4 arg4, Action<Exception>? @catch = null)
+    {
+        try
+        {
+            return await func(arg1, arg2, arg3, arg4);
         }
         catch (Exception ex)
         {
