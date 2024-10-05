f35bd070650941eca9e0d73a5419df141e10a338
ap-dev-at-home <AndreeP@gmx.de>
2024-10-05 15:13:08 +0200

    Add Do/Try Interlocked without result type

--- src/Results/ResultInterlocked.cs
+++ src/Results/ResultInterlocked.cs
@@ -23,6 +23,27 @@ public partial class Result
         }
     }
 
+    public static Result DoInterlocked(Func<Result> func, object l, bool wait = true)
+    {
+        if (wait == true)
+        {
+            Monitor.Enter(l);
+        }
+        else if (Monitor.TryEnter(l) == false)
+        {
+            return Result.Fail(new InterlockError());
+        }
+
+        try
+        {
+            return func();
+        }
+        finally
+        {
+            Monitor.Exit(l);
+        }
+    }
+
     public static Result<TResult> TryInterlocked<TResult>(Func<Result<TResult>> func, object l, bool wait = true, Action<Exception>? @catch = null)
     {
         if (wait == true)
@@ -42,6 +63,32 @@ public partial class Result
         {
             @catch?.Invoke(ex);
             return Result.Fail<TResult>(new ExceptionError(ex));
+        }
+        finally
+        {
+            Monitor.Exit(l);
+        }
+    }
+
+    public static Result TryInterlocked(Func<Result> func, object l, bool wait = true, Action<Exception>? @catch = null)
+    {
+        if (wait == true)
+        {
+            Monitor.Enter(l);
+        }
+        else if (Monitor.TryEnter(l) == false)
+        {
+            return Result.Fail(new InterlockError());
+        }
+
+        try
+        {
+            return func();
+        }
+        catch (Exception ex)
+        {
+            @catch?.Invoke(ex);
+            return Result.Fail(new ExceptionError(ex));
         }
         finally
         {
