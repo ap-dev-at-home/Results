ae6a8ca0b866a06347d2de258471cd9d78dd458e
ap-dev-at-home <AndreeP@gmx.de>
2024-10-10 22:41:21 +0200

    Split UnitTests Do/Try Interlocked

--- src/Results.Test/ResultTestDoInterlocked.cs
+++ src/Results.Test/ResultTestDoInterlocked.cs
@@ -209,21 +209,4 @@ public class ResultTestDoInterlocked
         Assert.IsTrue(result.Failed);
         Assert.IsInstanceOfType(result.Error, typeof(InterlockError));
     }
-
-    [TestMethod]
-    public void TryInterlockedReturnsResult()
-    {
-        var l = new object();
-        var result = Result.TryInterlocked(() => Result.Ok(), l);
-        Assert.IsTrue(result.Success);
-    }
-
-    [TestMethod]
-    public void TryInterlockedGenericReturnsResult()
-    {
-        var l = new object();
-        var result = Result.TryInterlocked(() => Result.Ok(1), l);
-        Assert.IsTrue(result.Success);
-        Assert.AreEqual(1, result.Value);
-    }
 }
