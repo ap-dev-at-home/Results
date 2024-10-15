c6eb5efac0ad17d16db85ae798d9e8989ef7c425
ap-dev-at-home <AndreeP@gmx.de>
2024-10-16 00:16:59 +0200

    Draft, ResultsJson

---
+++ src/Results/Json/ResultsJson.cs
@@ -0,0 +1,67 @@
+﻿using System.Text.Json;
+
+namespace Results.Json;
+
+public static class ResultsJson<T>
+{
+    public static Result<T> From(string jsonString)
+    {
+        try
+        {
+            T? obj = JsonSerializer.Deserialize<T>(jsonString);
+
+            return Result.Ok(obj);
+        }
+        catch (Exception ex)
+        {
+            return Result.Fail<T>(new ExceptionError(ex));
+        }
+    }
+
+    public static Result<T> Load(string path)
+    {
+        try
+        {
+            if (File.Exists(path) == false)
+            {
+                return Result.Fail<T>(new ExceptionError(new FileNotFoundException(path)));
+            }
+
+            T? obj = JsonSerializer.Deserialize<T>(File.ReadAllText(path));
+
+            return Result.Ok(obj);
+        }
+        catch (Exception ex)
+        {
+            return Result.Fail<T>(new ExceptionError(ex));
+        }
+    }
+
+    public static Result<T> From(Stream stream)
+    {
+        try
+        {
+            T? obj = JsonSerializer.Deserialize<T>(stream);
+
+            return Result.Ok(obj);
+        }
+        catch (Exception ex)
+        {
+            return Result.Fail<T>(new ExceptionError(ex));
+        }
+    }
+
+    public static Result<T> From(ReadOnlySpan<byte> utf8Json)
+    {
+        try
+        {
+            T? obj = JsonSerializer.Deserialize<T>(utf8Json);
+
+            return Result.Ok(obj);
+        }
+        catch (Exception ex)
+        {
+            return Result.Fail<T>(new ExceptionError(ex));
+        }
+    }
+}
