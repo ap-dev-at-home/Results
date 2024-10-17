b62166ce81b3e835aa3ead92ed1bac0b0f3f89b1
ap-dev-at-home <AndreeP@gmx.de>
2024-10-17 23:17:08 +0200

    ResultInterlocked, Inline Documentation

--- src/Results/ResultInterlocked.cs
+++ src/Results/ResultInterlocked.cs
@@ -2,6 +2,14 @@
 
 public partial class Result
 {
+    /// <summary>
+    /// Executes a function within an interlocked section, ensuring exclusive access to the provided lock object.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the result.</typeparam>
+    /// <param name="func">The function to execute.</param>
+    /// <param name="l">The lock object.</param>
+    /// <param name="wait">A flag indicating whether to wait for the lock or not.</param>
+    /// <returns>The result of the function execution.</returns>
     public static Result<TResult> DoInterlocked<TResult>(Func<Result<TResult>> func, object l, bool wait = true)
     {
         if (wait == true)
@@ -23,6 +31,13 @@ public partial class Result
         }
     }
 
+    /// <summary>
+    /// Executes a function within an interlocked section, ensuring exclusive access to the provided lock object.
+    /// </summary>
+    /// <param name="func">The function to execute.</param>
+    /// <param name="l">The lock object.</param>
+    /// <param name="wait">A flag indicating whether to wait for the lock or not.</param>
+    /// <returns>The result of the function execution.</returns>
     public static Result DoInterlocked(Func<Result> func, object l, bool wait = true)
     {
         if (wait == true)
@@ -44,6 +59,16 @@ public partial class Result
         }
     }
 
+    /// <summary>
+    /// Executes a function within an interlocked section, ensuring exclusive access to the provided lock object.
+    /// If an exception occurs during the function execution, it is caught and a failed result with the exception is returned.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the result.</typeparam>
+    /// <param name="func">The function to execute.</param>
+    /// <param name="l">The lock object.</param>
+    /// <param name="wait">A flag indicating whether to wait for the lock or not.</param>
+    /// <param name="catch">An optional action to handle the caught exception.</param>
+    /// <returns>The result of the function execution.</returns>
     public static Result<TResult> TryInterlocked<TResult>(Func<Result<TResult>> func, object l, bool wait = true, Action<Exception>? @catch = null)
     {
         if (wait == true)
@@ -70,6 +95,15 @@ public partial class Result
         }
     }
 
+    /// <summary>
+    /// Executes a function within an interlocked section, ensuring exclusive access to the provided lock object.
+    /// If an exception occurs during the function execution, it is caught and a failed result with the exception is returned.
+    /// </summary>
+    /// <param name="func">The function to execute.</param>
+    /// <param name="l">The lock object.</param>
+    /// <param name="wait">A flag indicating whether to wait for the lock or not.</param>
+    /// <param name="catch">An optional action to handle the caught exception.</param>
+    /// <returns>The result of the function execution.</returns>
     public static Result TryInterlocked(Func<Result> func, object l, bool wait = true, Action<Exception>? @catch = null)
     {
         if (wait == true)
